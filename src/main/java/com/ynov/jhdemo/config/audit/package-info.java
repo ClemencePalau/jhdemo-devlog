132627ee1e5391ce7a57398c20c4af31591a99d4
ClemencePalau <clemencepalau@gmail.com>
2020-11-19 15:38:24 +0100

    Initial version of jhdemo generated by JHipster-6.10.5

---
+++ src/main/java/com/ynov/jhdemo/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.ynov.jhdemo.config.audit;
---
+++ src/main/java/com/ynov/jhdemo/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.ynov.jhdemo.repository;
+
+import com.ynov.jhdemo.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
